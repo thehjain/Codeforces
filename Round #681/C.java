1bccde89e802f519a8517523dc643500877c9087
Harsh <thehjain@gmail.com>
2020-11-03 08:52:13 +0530

    After Contest

--- Round #681/C.java
+++ Round #681/C.java
@@ -1,9 +1,7 @@
-
-
-
 import java.util.*;
 import java.io.*;
 import java.math.*;
+
 
 public class C {
 
@@ -22,46 +20,64 @@ public class C {
 	static void solve() throws IOException {
 
 		int n = Integer.parseInt(br.readLine());
-		String[] str = br.readLine().trim().split(" ");
 
-		int[] arr = new int[n];
+		String[] arr = br.readLine().trim().split(" ");
+		String[] brr = br.readLine().trim().split(" ");
+
+		int[] A = new int[n];
+		int[] B = new int[n];
 		int index = 0;
 
-		for (String ele : str)
-			arr[index++] = Integer.parseInt(ele);
+		for (String ele : arr)
+			A[index++] = Integer.parseInt(ele);
+		index = 0;
+		for (String ele : brr)
+			B[index++] = Integer.parseInt(ele);
 
-		if (isPossible(arr, n))
-			System.out.println("YES");
-		else
-			System.out.println("NO");
+		long res = findSum(A, B, n);
+		System.out.println(res);
 
 	}
 
-	static boolean isPossible(int[] arr, int n) {
+	static long findSum(int[] arr, int[] brr, int n) {
 
-		ArrayList<Integer> res = new ArrayList<>();
-		ArrayList<Integer> res1 = new ArrayList<>();
+		Pair[] res = new Pair[n];
 
-		for (int i = 1; i < n - 1; i++) {
-
-			if ((arr[i] > arr[i - 1] && arr[i] > arr[i + 1]))
-				res.add(arr[i]);
-			if (arr[i] < arr[i - 1] && arr[i] < arr[i + 1])
-				res1.add(arr[i]);
+		for (int i = 0; i < n; i++) {
+			res[i] = new Pair(arr[i], brr[i]);
 		}
 
-		for (int i = 1; i < res.size() - 1; i++) {
-			if ((res.get(i) >= res.get(i - 1) && res.get(i) >= res.get(i + 1)) ||
-			        res.get(i) <= res.get(i - 1) && res.get(i) <= res.get(i + 1))
-				return false;
-		}
-		for (int i = 1; i < res1.size() - 1; i++) {
-			if ((res1.get(i) >= res1.get(i - 1) && res1.get(i) >= res1.get(i + 1)) ||
-			        res1.get(i) <= res1.get(i - 1) && res1.get(i) <= res1.get(i + 1))
-				return false;
-		}
+		Arrays.sort(res, new MyComparator());
 
-		return true;
+		long sum = 0;
+		long ans = Long.MAX_VALUE;
+
+		for (int i = n - 1; i >= 0; i--) {
+			long curr = Math.max(res[i].a, sum);
+
+			if (curr < ans)
+				ans = curr;
+
+			sum += res[i].b;
+
+		}
+		ans = Math.min(sum, ans);
+		return ans;
+
 	}
+}
 
+
+class Pair {
+	int a, b;
+	Pair(int a, int b) {
+		this.a = a;
+		this.b = b;
+	}
+}
+
+class MyComparator implements Comparator<Pair> {
+	public int compare(Pair A, Pair B) {
+		return (A.a - B.a);
+	}
 }
