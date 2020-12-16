06a9ccd06a63d3687b83e1065fa6196fbbbf6695
Harsh <thehjain@gmail.com>
2020-12-16 15:59:30 +0530

    After Contesst

--- Round #690/D.java
+++ Round #690/D.java
@@ -1,5 +1,5 @@
 
-
+//Add to Neihbour And Remove
 
 import java.util.*;
 import java.io.*;
@@ -21,7 +21,35 @@ public class D {
 
 	static void solve() throws IOException {
 
-		System.out.println("hello");
+		int n = Integer.parseInt(br.readLine());
+		String[] input = br.readLine().trim().split(" ");
+
+		int[] arr = new int[n];
+		int index = 0;
+
+		for (String ele : input)
+			arr[index++] = Integer.parseInt(ele);
+
+		int res = MAX;
+
+		int prefixSum = 0;
+		for (int i = 0; i < n; i++) {
+			prefixSum += arr[i];
+			int sum = 0;
+			int count = 0;
+			for (int j = i + 1; j < n; j++) {
+				sum += arr[j];
+				if (sum == prefixSum) {
+					count++;
+					sum = 0;
+				} else if (sum > prefixSum)
+					break;
+			}
+			if (sum == 0)
+				res = Math.min(res, n - count - 1);
+		}
+
+		System.out.println(res);
 
 	}
 
