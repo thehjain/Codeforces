81d119d8f7a58ad1e8a4fc127e8f077873ca8a40
Harsh <thehjain@gmail.com>
2020-12-17 20:08:03 +0530

    Initail Commit

---
+++ Edu Round #100/B.java
@@ -0,0 +1,28 @@
+
+
+
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class B {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static void main(String[] args) throws IOException {
+		int T = Integer.parseInt(br.readLine());
+		while (T-- > 0) {
+			solve();
+		}
+	}
+
+	static void solve() throws IOException {
+
+		System.out.println("hello");
+
+	}
+
+}
