375832c29a19d4801ff573e0abc1883a9c664d3e
Harsh <thehjain@gmail.com>
2020-12-17 22:21:52 +0530

    After Contest

--- Edu Round #100/B.java
+++ Edu Round #100/B.java
@@ -21,7 +21,26 @@ public class B {
 
 	static void solve() throws IOException {
 
-		System.out.println("hello");
+		int n = Integer.parseInt(br.readLine());
+		String[] input = br.readLine().trim().split(" ");
+
+		int[] arr = new int[n];
+		int index = 0;
+		for (String ele : input)
+			arr[index++] = Integer.parseInt(ele);
+
+		for (int i = 1; i < n; i++) {
+			if (!(arr[i] % arr[i - 1] == 0 || arr[i - 1] % arr[i] == 0)) {
+				if (arr[i] > arr[i - 1])
+					arr[i] -= arr[i] % arr[i - 1];
+				else
+					arr[i] = 1;
+			}
+		}
+
+		for (int ele : arr)
+			System.out.print(ele + " ");
+		System.out.println();
 
 	}
 
