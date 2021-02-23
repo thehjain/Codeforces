cd6e9d43919ab47c82d4990a754b246bcee5f02e
Harsh <thehjain@gmail.com>
2021-02-23 22:44:52 +0530

    During Contest

--- Round #704/C.java
+++ Round #704/C.java
@@ -4,19 +4,39 @@ import java.math.*;
 
 class C {
 
+	static class Pair {
+		int low;
+		int high;
+		Pair(int low, int high) {
+			this.low = low;
+			this.high = high;
+		}
+	}
+
 	private static int MAX = Integer.MAX_VALUE;
 	private static int MIN = Integer.MIN_VALUE;
 	private static int MOD = 1000000007;
 	static FastScanner sc = new FastScanner();
 
 	public static void main(String[] args) throws IOException {
-		int T = sc.nextInt();
-		while (T-- > 0) {
-			solve();
-		}
+		// int T = sc.nextInt();
+		// while (T-- > 0) {
+		solve();
+		// }
 	}
 
 	static void solve() throws IOException {
+
+		int n = sc.nextInt();
+		int m = sc.nextInt();
+
+		char[] s = sc.next().toCharArray();
+		char[] t = sc.next().toCharArray();
+
+		if (n == m) {
+			System.out.print(1);
+			return;
+		}
 
 
 
