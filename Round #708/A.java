6106cc003621d4d335f13669753eebf9312e3d55
Harsh <thehjain@gmail.com>
2021-03-17 20:36:43 +0530

    During Contest

---
+++ Round #708/A.java
@@ -0,0 +1,90 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class A {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+		System.out.print(sb);
+	}
+
+	static void solve() throws IOException {
+
+		int n = sc.nextInt();
+
+		int[] arr = new int[n];
+
+		for (int i = 0; i < n; i++)
+			arr[i] = sc.nextInt();
+
+		boolean[] visited = new boolean[n];
+
+		Arrays.sort(arr);
+
+		sb.append(arr[0] + " ");
+		visited[0] = true;
+
+		for (int i = 1; i < n; i++) {
+
+			if (arr[i] == arr[i - 1])
+				continue;
+			sb.append(arr[i] + " ");
+			visited[i] = true;
+
+		}
+
+		for (int i = 1; i < n; i++) {
+			if (!visited[i])
+				sb.append(arr[i] + " ");
+		}
+
+		sb.append("\n");
+
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
---
+++ Round #708/C.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+class C {
+
+	private static int MAX = Integer.MAX_VALUE;
+	private static int MIN = Integer.MIN_VALUE;
+	private static int MOD = 1000000007;
+	static FastScanner sc = new FastScanner();
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		int T = sc.nextInt();
+		while (T-- > 0) {
+			solve();
+		}
+		System.out.print(sb);
+	}
+
+	static void solve() throws IOException {
+
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+
+		sb.append((n - 2) + " " + 1 + " " + 1);
+
+		sb.append("\n");
+
+	}
+
+	static class FastScanner {
+		public BufferedReader reader;
+		public StringTokenizer tokenizer;
+		public FastScanner() {
+			reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+			tokenizer = null;
+		}
+		public String next() {
+			while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+				try {
+					tokenizer = new StringTokenizer(reader.readLine());
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+			return tokenizer.nextToken();
+		}
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		public String nextLine() {
+			try {
+				return reader.readLine();
+			} catch (IOException e) {
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+}
